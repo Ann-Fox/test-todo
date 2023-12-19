f2dc820a077f4121d5eb588ea42da0689429869a
Anna Chernysheva <dx8565@gmail.com>
2023-12-19 15:01:44 +0500

    Develop/popup branch commit 1

--- User/User.tsx
+++ User/User.tsx
@@ -7,6 +7,7 @@ export const User () => {
             <div>Dev branch commit 1</div>
             <div>Dev branch commit 2</div>
             <div>Dev branch commit 3</div>
+            <div>Develop/popup branch commit 1</div>
         </div>
     )
 }
