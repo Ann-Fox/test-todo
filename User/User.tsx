7b5b44da46c5ac4c5a0276f00a3db3db9edde38b
Anna Chernysheva <dx8565@gmail.com>
2023-12-19 14:58:27 +0500

    Develop/login branch commit 2

--- User/User.tsx
+++ User/User.tsx
@@ -8,6 +8,7 @@ export const User () => {
             <div>Dev branch commit 2</div>
             <div>Dev branch commit 3</div>
             <div>Develop/login branch commit 1</div>
+            <div>Develop/login branch commit 2</div>
         </div>
     )
 }
